5751fb9784d36ab839ecf3c015cab4f672b1078d
Viktor Ovcharov <ovcharov.2v@gmail.com>
2020-10-31 15:34:22 +0400

    feat: complete Vigenera cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,53 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(reverse) {
+    this.reverse = reverse || reverse == '';
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+
+  encrypt(message, key) {
+    return this.crypt(message, key, 'encrypt');
+  }
+
+  decrypt(message, key) {
+    return this.crypt(message, key, 'decrypt');
+  }
+
+  crypt(message, key, action) {
+    if (message && key) {
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+
+      let resultMessage = [];
+      let keywordIndex = 0;
+
+      for (let symbol of message) {
+        if (this.indexInAlphabet(symbol) >= 0 && this.indexInAlphabet(symbol) <= this.alphabet.length - 1) {
+          let transformedSymbol;
+          if (action === 'encrypt') {
+            transformedSymbol = (this.indexInAlphabet(symbol) + this.indexInAlphabet(key[keywordIndex]));
+          } else {
+            transformedSymbol = (this.indexInAlphabet(symbol) + this.alphabet.length - this.indexInAlphabet(key[keywordIndex]));
+          }
+          transformedSymbol = transformedSymbol % this.alphabet.length;
+
+          resultMessage.push(this.alphabet[transformedSymbol]);
+          keywordIndex++;
+        } else resultMessage.push(symbol);
+
+        if (keywordIndex === key.length) keywordIndex = 0;
+      }
+
+      if (this.reverse) return resultMessage.reverse().join('');
+      return resultMessage.join('');
+    } else {
+      throw new Error();
+    }
+  }
+
+  indexInAlphabet(transformedSymbol) {
+    return this.alphabet.indexOf(transformedSymbol);
   }
 }
 
