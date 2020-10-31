ee8936f2394053ce04998d5a0c7009db1112908e
Viktor Ovcharov <ovcharov.2v@gmail.com>
2020-10-31 14:43:42 +0400

    feat: complete Tarsform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function trresultform(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-prev':
+        if (i > 0) result.pop();
+        break;
+      case '--double-prev':
+        if (i > 0) result.push(arr[i - 1]);
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) result.push(arr[i + 1]);
+        break;
+      case '--discard-next':
+        if ((arr[i + 2] == '--double-prev' || arr[i + 2] == '--discard-prev')) i += 2
+        else i++
+        break;
+      default:
+        result.push(arr[i])
+    }
+  }
+  return result;
 };
